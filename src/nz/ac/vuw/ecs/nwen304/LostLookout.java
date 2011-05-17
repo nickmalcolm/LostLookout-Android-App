b28caba19f5d7787e7e0d588d4cbcd282ac38854
Nick Malcolm <nick@southgatelabs.com>
2011-05-18 00:19:57 +1200

    Base Android App

---
+++ src/nz/ac/vuw/ecs/nwen304/LostLookout.java
@@ -0,0 +1,13 @@
+package nz.ac.vuw.ecs.nwen304;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LostLookout extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
