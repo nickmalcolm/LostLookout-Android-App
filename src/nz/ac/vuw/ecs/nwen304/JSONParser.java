6dc678c60d52431e485c6035000ed30d266a460a
Nick Malcolm <nick@southgatelabs.com>
2011-05-18 22:07:56 +1200

    Adds a JSON parser and a POJO

---
+++ src/nz/ac/vuw/ecs/nwen304/JSONParser.java
@@ -0,0 +1,14 @@
+package nz.ac.vuw.ecs.nwen304;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONParser {
+	
+	private List<Listing> listings = new ArrayList<Listing>();
+	
+	public void parseListings(String jsonURL){
+		
+	}
+
+}
---
+++ src/nz/ac/vuw/ecs/nwen304/Listing.java
@@ -0,0 +1,11 @@
+package nz.ac.vuw.ecs.nwen304;
+
+public class Listing {
+
+	public String title;
+	public double latitude;
+	public double longitude;
+	public String description;
+	public String url;
+
+}
