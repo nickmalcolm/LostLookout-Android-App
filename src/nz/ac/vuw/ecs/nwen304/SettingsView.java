2e32395892498c4607a40090395804a299436601
Nick Malcolm <nick@southgatelabs.com>
2011-05-27 18:14:31 +1200

    Adds basic Settings view

---
+++ src/nz/ac/vuw/ecs/nwen304/SettingsView.java
@@ -0,0 +1,14 @@
+package nz.ac.vuw.ecs.nwen304;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SettingsView extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
+	}
+
+}
