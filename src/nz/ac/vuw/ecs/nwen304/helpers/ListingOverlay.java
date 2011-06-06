ccb78717c3ebe5a04bb4ea29c3d1e4eec8b2d8e0
Nick Malcolm <nick@southgatelabs.com>
2011-06-06 18:30:54 +1200

    Shows icon in alert

--- src/nz/ac/vuw/ecs/nwen304/helpers/ListingOverlay.java
+++ src/nz/ac/vuw/ecs/nwen304/helpers/ListingOverlay.java
@@ -1,9 +1,13 @@
 package nz.ac.vuw.ecs.nwen304.helpers;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import nz.ac.vuw.ecs.nwen304.ListingWebView;
-
+import nz.ac.vuw.ecs.nwen304.R;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -11,6 +15,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ScaleDrawable;
 import android.util.Log;
 
 import com.google.android.maps.ItemizedOverlay;
@@ -56,6 +61,11 @@ public class ListingOverlay extends ItemizedOverlay<OverlayItem> {
 	  dialog.setTitle(item.getTitle());
 	  dialog.setMessage(item.getSnippet());
 	  dialog.setCancelable(false);
+
+	  Drawable image = imageOperations(mContext, "http://i.imgur.com/6LRWis.jpg");
+	  if(image != null){
+		  dialog.setIcon(image);
+	  }
 	  
 	  //Add button to view on website, and to cancel dialog box.
 	  
@@ -78,6 +88,27 @@ public class ListingOverlay extends ItemizedOverlay<OverlayItem> {
 	  
 	}
 	
+	private Drawable imageOperations(Context mContext2, String url) {
+		// TODO Auto-generated method stub
+		try {
+			InputStream is = (InputStream) this.fetch(url);
+			Drawable d = Drawable.createFromStream(is, "src");
+			return d;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public Object fetch(String address) throws MalformedURLException,IOException {
+		URL url = new URL(address);
+		Object content = url.getContent();
+		return content;
+	}
+
 	/**
 	 * Saves an URL in the shared preferences. Used for ListingWebView
 	 */
